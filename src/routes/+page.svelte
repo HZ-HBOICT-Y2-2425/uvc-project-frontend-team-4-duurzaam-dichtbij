bc3320a02d3cdf35e75315ae91663d199787284e
ThianWarringtonHZ <warr0006@hz.nl>
2024-11-27 16:27:22 +0100

    Made the map load real geo locations and added loading screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
     import { onMount } from "svelte";
     import "leaflet/dist/leaflet.css";
 
+    /**
+     * @type {{ on: (arg0: string, arg1: (e: { latlng: any; }) => void) => void; }}
+     */
     let map;
     onMount(async () => {
         const L = await import("leaflet");
@@ -14,18 +17,64 @@
             attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }).addTo(map);
 
-        // Add a marker
-        L.marker([51.533370759239155, 3.9179992675781254]).addTo(map).bindPopup("A pretty popup!").openPopup();
+        // Load addresses
+        const addresses = [
+            {
+                name: 'Wynkel',
+                address: 'Botterlaan 39',
+                city: 'Kortgene'
+            },
+            {
+                name: 'Wynkel',
+                address: 'Sint Janskerke 4A',
+                city: 'Zoutelande'
+            },
+            {
+                name: 'Wynkel',
+                address: 'Kruitmolenlaan 9',
+                city: 'Middelburg'
+            },
+        ];
 
-        map.on('click', function (/** @type {{ latlng: any; }} */ e) {
-            const coord = e.latlng;
-            const lat = coord.lat;
-            const lng = coord.lng;
-            console.log(`${lat} ${lng}`);
+        const points = [];
+        for (let i = 0; i < addresses.length; i++) {
+            const address = addresses[i];
+            let lat = null;
+            let lng = null;
+            const response = await fetch(`https://nominatim.openstreetmap.org/search?street=${address.address}&city=${address.city}&format=jsonv2`).then(res => {
+                return res.json();
+            }).then(json => {
+                console.log(json);
+                lat = json[0].lat;
+                lng = json[0].lon;
+                console.log(`${lat} ${lng}`)
+            }).catch(error => {
+                console.error(error);
+            });
+
+            console.log(`Found latlng ${lat} ${lng}`);
+            points.push({
+                name: address.name,
+                lat: lat,
+                lng: lng,
+            });
+        }
+        points.forEach(point => {
+            // Add a marker
+            L.marker([point.lat, point.lng]).addTo(map).bindPopup(point.name);
         });
+
+        const loadImg = document.getElementById('loading');
+        if (loadImg) {
+            loadImg.style.display = "none";
+        }
     });
 </script>
 
+<div id="loading">
+    <img src="https://media.tenor.com/_62bXB8gnzoAAAAj/loading.gif" alt="loading">
+    <p>Loading Map</p>
+</div>
 <div id="map"></div>
 
 <style>
@@ -33,6 +82,27 @@
         flex: 1;
         height: 100%;
         position: relative;
+    }
+
+    #loading {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 4000;
+        background-color: rgba(0, 0, 0, 0.2);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        gap: 10px;
+        padding: 10px;
+        border-radius: 8px;
+    }
+
+    #loading img {
+        width: 50px;
+        height: 50px;
     }
 
     :global(#page) {
