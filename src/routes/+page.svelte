f66adacb3704d9f02fde54b576922593a5632a64
ThianWarringtonHZ <warr0006@hz.nl>
2024-11-27 15:02:27 +0100

    Created initial layout and pages. Added basic map functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,41 @@
 <script>
-    import "../app.css";
+    import { onMount } from "svelte";
+    import "leaflet/dist/leaflet.css";
+
+    let map;
+    onMount(async () => {
+        const L = await import("leaflet");
+
+        // Initialize map
+        map = L.map('map').setView([51.533370759239155, 3.9179992675781254], 10);
+
+        // Add a tile layer
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+
+        // Add a marker
+        L.marker([51.533370759239155, 3.9179992675781254]).addTo(map).bindPopup("A pretty popup!").openPopup();
+
+        map.on('click', function (/** @type {{ latlng: any; }} */ e) {
+            const coord = e.latlng;
+            const lat = coord.lat;
+            const lng = coord.lng;
+            console.log(`${lat} ${lng}`);
+        });
+    });
 </script>
 
-<h1 class="bg-lime-400">Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+<div id="map"></div>
+
+<style>
+    #map {
+        flex: 1;
+        height: 100%;
+        position: relative;
+    }
+
+    :global(#page) {
+        flex: auto !important;
+    }
+</style>
