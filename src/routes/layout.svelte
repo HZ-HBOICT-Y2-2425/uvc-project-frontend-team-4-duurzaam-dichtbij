f5a25ba98a26ce1f2a85bb91ec714dc6e67e2c80
ThianWarringtonHZ <warr0006@hz.nl>
2024-12-09 12:53:40 +0100

    Added TailwindCSS

--- src/routes/layout.svelte
+++ src/routes/layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from "svelte";
+    import "../app.css";
 
     let sidebarVisible = false; // Sidebar visibility state
     let menubarVisible = false; // Menubar visibility state (only for mobile)
